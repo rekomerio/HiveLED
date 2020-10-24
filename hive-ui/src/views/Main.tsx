dbd33c94da4d7ee95e7fae00e8ee9b5de2c84ac5
Reko <K9260@student.jamk.fi>
2020-10-24 20:35:31 +0300

    Testing...

--- hive-ui/src/views/Main.tsx
+++ hive-ui/src/views/Main.tsx
@@ -1,73 +1,94 @@
 import React, { useRef, useState, useEffect } from "react";
 import { Box, makeStyles } from "@material-ui/core";
 import Client from "../components/Client";
-import { Option } from "../helpers/types";
+import { Command, Option } from "../helpers/types";
+import { constructMessage } from "../utils/message";
 
 export interface MainProps {}
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-        height: "100%",
-        minHeight: "100vh",
-        backgroundColor: theme.palette.background.default,
-    },
+  root: {
+    height: "100%",
+    minHeight: "100vh",
+    backgroundColor: theme.palette.background.default,
+  },
 }));
 
 const Main = () => {
-    const classes = useStyles();
-    const [isConnected, setIsConnected] = useState<boolean>(false);
-    const [effects, setEffects] = useState<Option[]>([]);
-    const socket = useRef<WebSocket>(new WebSocket("ws://192.168.43.26:81"));
+  const classes = useStyles();
+  const [isConnected, setIsConnected] = useState<boolean>(false);
+  const [effects, setEffects] = useState<Option[]>([]);
+  const [params, setParams] = useState<Option[][]>([]);
+  const socket = useRef<WebSocket>(new WebSocket("ws://192.168.43.26:81"));
 
-    useEffect(() => {
-        socket.current.addEventListener("open", (e) => {
-            console.log("connected", e);
-            setIsConnected(true);
+  useEffect(() => {
+    socket.current.addEventListener("open", (e) => {
+      console.log("connected", e);
+      setIsConnected(true);
+    });
+
+    socket.current.addEventListener("close", (e) => {
+      console.log("disconnected", e);
+      setIsConnected(false);
+    });
+
+    socket.current.addEventListener("message", (e) => {
+      console.log("new message", e);
+
+      if (typeof e.data === "object") {
+        e.data.bufferArray().then((res) => {
+          const arr = new Uint8Array(res);
+          console.log(arr);
         });
-
-        socket.current.addEventListener("close", (e) => {
-            console.log("disconnected", e);
-            setIsConnected(false);
+      } else if (
+        typeof e.data === "string" &&
+        (e.data.startsWith("[") || e.data.startsWith("{"))
+      ) {
+        const obj = JSON.parse(e.data);
+        console.log(obj);
+        if (Object.keys(obj)[0].includes("params")) {
+          console.log("params received", Object.keys(obj)[0].split("@"));
+          const clientId = Object.keys(obj)[0].split("@")[0];
+          setParams((state) =>
+            state.map((params, i) => {
+              if (i === +clientId) return {};
+            })
+          );
+        }
+        /*
+        const options: Option[] = JSON.parse(e.data).map((x) => {
+          return {
+            name: x[Object.keys(x)[0]],
+            value: parseInt(Object.keys(x)[0]),
+          };
         });
+*/
+        //setEffects(options);
+      }
+    });
+  }, []);
 
-        socket.current.addEventListener("message", (e) => {
-            console.log("new message", e);
+  useEffect(() => {
+    if (isConnected) {
+      const message = constructMessage(Command.GetParams, 0, 0, 0);
+      socket.current.send(message);
+    }
+  }, [isConnected]);
 
-            if (typeof e.data === "object") {
-                e.data.bufferArray().then((res) => {
-                    const arr = new Uint8Array(res);
-                    console.log(arr);
-                });
-            } else if (typeof e.data === "string" && e.data.startsWith("[")) {
-                console.log(JSON.parse(e.data));
-
-                const options: Option[] = Object.keys(JSON.parse(e.data)).reduce(
-                    (options, key) => {
-                        options.push({ name: e.data[key], value: parseInt(key) });
-                        return options;
-                    },
-                    []
-                );
-
-                setEffects(options);
-            }
-        });
-    }, []);
-
-    return (
-        <div className={classes.root}>
-            {[0, 1].map((client, i) => (
-                <Box key={i} padding={2}>
-                    <Client
-                        id={i}
-                        socket={socket.current}
-                        isConnected={isConnected}
-                        effects={effects}
-                    />
-                </Box>
-            ))}
-        </div>
-    );
+  return (
+    <div className={classes.root}>
+      {[0, 1].map((client, i) => (
+        <Box key={i} padding={2}>
+          <Client
+            id={i}
+            socket={socket.current}
+            isConnected={isConnected}
+            effects={effects}
+          />
+        </Box>
+      ))}
+    </div>
+  );
 };
 
 export default Main;

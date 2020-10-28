08a25e5d43b58d5e1974828d8f289ee96c0d7171
Reko Meriö <K9260@student.jamk.fi>
2020-10-28 21:25:57 +0200

    Updates

---
+++ precache-manifest.68712043efa1a801f1859817a13cd897.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb24e23b10f174bcb2a3f3ef94125308",
+    "url": "/HiveLED/index.html"
+  },
+  {
+    "revision": "f59314a103c7c77e5c35",
+    "url": "/HiveLED/static/css/main.d644109b.chunk.css"
+  },
+  {
+    "revision": "af4a9b918ef8d7ad2002",
+    "url": "/HiveLED/static/js/2.157c1fe0.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/HiveLED/static/js/2.157c1fe0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f59314a103c7c77e5c35",
+    "url": "/HiveLED/static/js/main.c2005347.chunk.js"
+  },
+  {
+    "revision": "938a37c011684b67b170",
+    "url": "/HiveLED/static/js/runtime-main.d64e29fd.js"
+  }
+]);

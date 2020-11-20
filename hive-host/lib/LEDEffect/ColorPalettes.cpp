c4ef4d31501cced16150e8ad167085918661ca8c
Reko Meriö <K9260@student.jamk.fi>
2020-11-21 00:34:32 +0200

    New palettes

--- hive-host/lib/LEDEffect/ColorPalettes.cpp
+++ hive-host/lib/LEDEffect/ColorPalettes.cpp
@@ -73,15 +73,290 @@ DEFINE_GRADIENT_PALETTE(wave_gp){
     212, 88, 0, 92,
     255, 255, 22, 16};
 
+DEFINE_GRADIENT_PALETTE(bhw1_28_gp){
+    0, 75, 1, 221,
+    30, 252, 73, 255,
+    48, 169, 0, 242,
+    119, 0, 149, 242,
+    170, 43, 0, 242,
+    206, 252, 73, 255,
+    232, 78, 12, 214,
+    255, 0, 149, 242};
+
+// Gradient palette "bhw1_14_gp", originally from
+// http://soliton.vm.bytemark.co.uk/pub/cpt-city/bhw/bhw1/tn/bhw1_14.png.index.html
+// converted for FastLED with gammas (2.6, 2.2, 2.5)
+// Size: 36 bytes of program space.
+
+DEFINE_GRADIENT_PALETTE(bhw1_14_gp){
+    0, 0, 0, 0,
+    12, 1, 1, 3,
+    53, 8, 1, 22,
+    80, 4, 6, 89,
+    119, 2, 25, 216,
+    145, 7, 10, 99,
+    186, 15, 2, 31,
+    233, 2, 1, 5,
+    255, 0, 0, 0};
+
+// Gradient palette "bhw1_22_gp", originally from
+// http://soliton.vm.bytemark.co.uk/pub/cpt-city/bhw/bhw1/tn/bhw1_22.png.index.html
+// converted for FastLED with gammas (2.6, 2.2, 2.5)
+// Size: 24 bytes of program space.
+
+DEFINE_GRADIENT_PALETTE(bhw1_22_gp){
+    0, 1, 1, 1,
+    45, 30, 10, 1,
+    96, 60, 9, 1,
+    130, 197, 36, 12,
+    188, 30, 10, 1,
+    255, 1, 1, 1};
+
+// Gradient palette "bhw1_23_gp", originally from
+// http://soliton.vm.bytemark.co.uk/pub/cpt-city/bhw/bhw1/tn/bhw1_23.png.index.html
+// converted for FastLED with gammas (2.6, 2.2, 2.5)
+// Size: 20 bytes of program space.
+
+DEFINE_GRADIENT_PALETTE(bhw1_23_gp){
+    0, 83, 6, 1,
+    84, 194, 52, 1,
+    122, 182, 105, 34,
+    165, 194, 52, 1,
+    255, 83, 6, 1};
+
+// Gradient palette "bhw1_05_gp", originally from
+// http://soliton.vm.bytemark.co.uk/pub/cpt-city/bhw/bhw1/tn/bhw1_05.png.index.html
+// converted for FastLED with gammas (2.6, 2.2, 2.5)
+// Size: 8 bytes of program space.
+
+DEFINE_GRADIENT_PALETTE(bhw1_05_gp){
+    0, 1, 221, 53,
+    255, 73, 3, 178};
+
+// Gradient palette "bhw2_greenman_gp", originally from
+// http://soliton.vm.bytemark.co.uk/pub/cpt-city/bhw/bhw2/tn/bhw2_greenman.png.index.html
+// converted for FastLED with gammas (2.6, 2.2, 2.5)
+// Size: 12 bytes of program space.
+
+DEFINE_GRADIENT_PALETTE(bhw2_greenman_gp){
+    0, 1, 22, 1,
+    130, 1, 168, 2,
+    255, 1, 22, 1};
+
+// Gradient palette "bhw2_57_gp", originally from
+// http://soliton.vm.bytemark.co.uk/pub/cpt-city/bhw/bhw2/tn/bhw2_57.png.index.html
+// converted for FastLED with gammas (2.6, 2.2, 2.5)
+// Size: 28 bytes of program space.
+
+DEFINE_GRADIENT_PALETTE(bhw2_57_gp){
+    0, 0, 0, 0,
+    43, 14, 2, 5,
+    81, 40, 1, 7,
+    130, 182, 1, 1,
+    173, 40, 1, 7,
+    214, 14, 2, 5,
+    255, 0, 0, 0};
+
+// Gradient palette "bhw3_32_gp", originally from
+// http://soliton.vm.bytemark.co.uk/pub/cpt-city/bhw/bhw3/tn/bhw3_32.png.index.html
+// converted for FastLED with gammas (2.6, 2.2, 2.5)
+// Size: 52 bytes of program space.
+
+DEFINE_GRADIENT_PALETTE(bhw3_32_gp){
+    0, 234, 231, 1,
+    15, 171, 43, 6,
+    40, 121, 0, 0,
+    53, 95, 1, 29,
+    71, 73, 1, 168,
+    94, 38, 63, 221,
+    109, 115, 51, 221,
+    127, 38, 63, 221,
+    147, 73, 1, 168,
+    181, 203, 28, 1,
+    193, 155, 16, 11,
+    216, 73, 1, 168,
+    255, 1, 4, 29};
+
+// Gradient palette "bhw3_07_gp", originally from
+// http://soliton.vm.bytemark.co.uk/pub/cpt-city/bhw/bhw3/tn/bhw3_07.png.index.html
+// converted for FastLED with gammas (2.6, 2.2, 2.5)
+// Size: 44 bytes of program space.
+
+DEFINE_GRADIENT_PALETTE(bhw3_07_gp){
+    0, 132, 5, 1,
+    30, 12, 13, 147,
+    53, 54, 32, 166,
+    73, 103, 39, 98,
+    84, 239, 52, 27,
+    96, 71, 2, 1,
+    117, 46, 8, 33,
+    130, 54, 32, 166,
+    163, 10, 7, 78,
+    196, 239, 52, 27,
+    255, 1, 1, 14};
+
+// Gradient palette "velvet_ocean_gp", originally from
+// http://soliton.vm.bytemark.co.uk/pub/cpt-city/colo/Skyblue2u/tn/velvet_ocean.png.index.html
+// converted for FastLED with gammas (2.6, 2.2, 2.5)
+// Size: 40 bytes of program space.
+
+DEFINE_GRADIENT_PALETTE(velvet_ocean_gp){
+    0, 1, 79, 80,
+    51, 1, 79, 80,
+    51, 1, 62, 62,
+    102, 1, 62, 62,
+    102, 1, 43, 40,
+    153, 1, 43, 40,
+    153, 1, 27, 23,
+    204, 1, 27, 23,
+    204, 1, 13, 10,
+    255, 1, 13, 10};
+
+// Gradient palette "deeper_than_the_sea_gp", originally from
+// http://soliton.vm.bytemark.co.uk/pub/cpt-city/colo/Skyblue2u/tn/deeper_than_the_sea.png.index.html
+// converted for FastLED with gammas (2.6, 2.2, 2.5)
+// Size: 40 bytes of program space.
+
+DEFINE_GRADIENT_PALETTE(deeper_than_the_sea_gp){
+    0, 0, 7, 29,
+    51, 0, 7, 29,
+    51, 0, 5, 19,
+    102, 0, 5, 19,
+    102, 0, 3, 13,
+    153, 0, 3, 13,
+    153, 0, 1, 4,
+    204, 0, 1, 4,
+    204, 0, 1, 1,
+    255, 0, 1, 1};
+
+// Gradient palette "The_Calanques_gp", originally from
+// http://soliton.vm.bytemark.co.uk/pub/cpt-city/colo/angelafaye/tn/The_Calanques.png.index.html
+// converted for FastLED with gammas (2.6, 2.2, 2.5)
+// Size: 40 bytes of program space.
+
+DEFINE_GRADIENT_PALETTE(The_Calanques_gp){
+    0, 1, 6, 14,
+    145, 1, 6, 14,
+    145, 1, 42, 66,
+    160, 1, 42, 66,
+    160, 2, 73, 52,
+    173, 2, 73, 52,
+    173, 12, 95, 39,
+    188, 12, 95, 39,
+    188, 92, 156, 96,
+    255, 92, 156, 96};
+
+// Gradient palette "natural_light_gp", originally from
+// http://soliton.vm.bytemark.co.uk/pub/cpt-city/colo/alpen/tn/natural_light.png.index.html
+// converted for FastLED with gammas (2.6, 2.2, 2.5)
+// Size: 40 bytes of program space.
+
+DEFINE_GRADIENT_PALETTE(natural_light_gp){
+    0, 247, 35, 7,
+    89, 247, 35, 7,
+    89, 120, 6, 24,
+    99, 120, 6, 24,
+    99, 15, 1, 29,
+    119, 15, 1, 29,
+    119, 1, 1, 23,
+    173, 1, 1, 23,
+    173, 1, 1, 9,
+    255, 1, 1, 9};
+
+// Gradient palette "refuel_gp", originally from
+// http://soliton.vm.bytemark.co.uk/pub/cpt-city/colo/alpen/tn/refuel.png.index.html
+// converted for FastLED with gammas (2.6, 2.2, 2.5)
+// Size: 40 bytes of program space.
+
+DEFINE_GRADIENT_PALETTE(refuel_gp){
+    0, 2, 252, 88,
+    51, 2, 252, 88,
+    51, 2, 151, 80,
+    79, 2, 151, 80,
+    79, 3, 82, 69,
+    153, 3, 82, 69,
+    153, 20, 13, 21,
+    165, 20, 13, 21,
+    165, 5, 5, 9,
+    255, 5, 5, 9};
+
+// Gradient palette "Sleep_Deprevation_gp", originally from
+// http://soliton.vm.bytemark.co.uk/pub/cpt-city/colo/vredeling/tn/Sleep_Deprevation.png.index.html
+// converted for FastLED with gammas (2.6, 2.2, 2.5)
+// Size: 40 bytes of program space.
+
+DEFINE_GRADIENT_PALETTE(Sleep_Deprevation_gp){
+    0, 21, 3, 56,
+    51, 21, 3, 56,
+    51, 39, 21, 145,
+    102, 39, 21, 145,
+    102, 43, 47, 170,
+    153, 43, 47, 170,
+    153, 77, 105, 197,
+    204, 77, 105, 197,
+    204, 146, 184, 223,
+    255, 146, 184, 223};
+
+// Gradient palette "All_kinds_of_green_gp", originally from
+// http://soliton.vm.bytemark.co.uk/pub/cpt-city/colo/smorin2002/tn/All_kinds_of_green.png.index.html
+// converted for FastLED with gammas (2.6, 2.2, 2.5)
+// Size: 40 bytes of program space.
+
+DEFINE_GRADIENT_PALETTE(All_kinds_of_green_gp){
+    0, 203, 248, 95,
+    51, 203, 248, 95,
+    51, 135, 197, 78,
+    102, 135, 197, 78,
+    102, 78, 197, 49,
+    153, 78, 197, 49,
+    153, 39, 173, 52,
+    204, 39, 173, 52,
+    204, 21, 105, 36,
+    255, 21, 105, 36};
+
+// Gradient palette "To_My_1st_100_Loves_gp", originally from
+// http://soliton.vm.bytemark.co.uk/pub/cpt-city/colo/rphnick/tn/To_My_1st_100_Loves.png.index.html
+// converted for FastLED with gammas (2.6, 2.2, 2.5)
+// Size: 40 bytes of program space.
+
+DEFINE_GRADIENT_PALETTE(To_My_1st_100_Loves_gp){
+    0, 157, 8, 5,
+    51, 157, 8, 5,
+    51, 77, 4, 2,
+    102, 77, 4, 2,
+    102, 42, 1, 1,
+    153, 42, 1, 1,
+    153, 13, 1, 1,
+    204, 13, 1, 1,
+    204, 1, 1, 1,
+    255, 1, 1, 1};
+
 // Remember to add palette to /data/json/palettes.json
 extern const TProgmemRGBGradientPalettePtr hiveColorPalettes[] = {
-    wave_gp,
-    bhw1_purplered_gp,
-    rgi_15_gp,
-    bhw3_61_gp,
-    bhw3_02_gp,
-    bhw3_11_gp,
+    bhw3_21_gp,
     bhw2_turq_gp,
-    bhw3_21_gp};
+    bhw3_11_gp,
+    bhw3_02_gp,
+    bhw3_61_gp,
+    rgi_15_gp,
+    bhw1_purplered_gp,
+    wave_gp,
+    bhw1_28_gp,
+    bhw1_14_gp,
+    bhw1_22_gp,
+    bhw1_23_gp,
+    bhw1_05_gp,
+    bhw2_greenman_gp,
+    bhw2_57_gp,
+    bhw3_32_gp,
+    bhw3_07_gp,
+    velvet_ocean_gp,
+    deeper_than_the_sea_gp,
+    The_Calanques_gp,
+    natural_light_gp,
+    refuel_gp,
+    Sleep_Deprevation_gp,
+    All_kinds_of_green_gp,
+    To_My_1st_100_Loves_gp};
 
 extern const uint8_t hiveColorPaletteCount = sizeof(hiveColorPalettes) / sizeof(TProgmemRGBGradientPalettePtr);
